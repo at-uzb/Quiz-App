63f3205ddab5c4671fe749ab2b77a6531f2baf23
Abdulaziz <74441512+at-uzb@users.noreply.github.com>
2025-01-09 20:49:26 +0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tothebest2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'odnw itjg zeca dtxr'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 SIMPLE_JWT = {
